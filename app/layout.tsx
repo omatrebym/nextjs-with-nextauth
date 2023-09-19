42f709b9a56518664c47c3fed7404e95a6ac5f8f
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-19 15:54:03 +0200

    Legg til favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,18 @@ import type { Metadata } from "next";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
+import Favicon from "@oslokommune/punkt-assets/dist/logos/favicon.ico";
+import Shortcut from "@oslokommune/punkt-assets/dist/logos/16x16-favicon.png";
+import Apple from "@oslokommune/punkt-assets/dist/logos/apple-touch-icon.png";
+
 export const metadata: Metadata = {
   title: "Tester NextJS",
   description: "Beskrivelse av testing av NextJS",
+  icons: {
+    icon: Favicon.src,
+    shortcut: Shortcut.src,
+    apple: Apple.src,
+  },
 };
 
 export default function RootLayout({

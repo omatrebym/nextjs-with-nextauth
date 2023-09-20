38163431890a69858ae34d4b9344b17d7ead01fc
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-20 12:58:19 +0200

    Override text i tw

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div>hei</div>;
+  return (
+    <div>
+      <h1 className="tw-text-h1">Tester NextJS</h1>
+      <h2 className="tw-text-h2">Enkel tittel</h2>
+      <h3 className="tw-text-h3">Enkel undertittel</h3>
+      <p>Et eller annet...</p>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { osloTextSizePlugin } from "./tw-plugins/typography";
 
 const config: Config = {
   prefix: "tw-",
@@ -10,33 +11,48 @@ const config: Config = {
     "*.html",
   ],
   theme: {
-    extend: {
-      breakpoints: {
-        // min-width
-        mobile: "0px",
-        phablet: "576px",
-        tablet: "768px",
-        "tablet-big": "1024px",
-        laptop: "1280px",
-        desktop: "1600px",
-        // max-width
-        "phablet-max": { max: "576px" },
-        "tablet-max": { max: "768px" },
-        "tablet-big-max": { max: "1024px" },
-        "laptop-max": { max: "1280px" },
-        "desktop-max": { max: "1600px" },
-        // target spesific screen size
-        "mobile-only": { min: "0px", max: "576px" },
-        "phablet-only": { min: "576px", max: "768px" },
-        "tablet-only": { min: "768px", max: "1024px" },
-        "tablet-big-only": { min: "1024px", max: "1280px" },
-        "laptop-only": { min: "1280px", max: "1600px" },
-      },
-      fontFamily: {
-        sans: ["Oslo sans", "arial", "sans-serif"],
-      },
+    screens: {
+      // min-width
+      mobile: "0px",
+      phablet: "576px",
+      tablet: "768px",
+      "tablet-big": "1024px",
+      laptop: "1280px",
+      desktop: "1600px",
+      // max-width
+      "phablet-max": { max: "576px" },
+      "tablet-max": { max: "768px" },
+      "tablet-big-max": { max: "1024px" },
+      "laptop-max": { max: "1280px" },
+      "desktop-max": { max: "1600px" },
+      // target spesific screen size
+      "mobile-only": { min: "0px", max: "576px" },
+      "phablet-only": { min: "576px", max: "768px" },
+      "tablet-only": { min: "768px", max: "1024px" },
+      "tablet-big-only": { min: "1024px", max: "1280px" },
+      "laptop-only": { min: "1280px", max: "1600px" },
     },
+    fontFamily: {
+      sans: ["Oslo sans", "arial", "sans-serif"],
+    },
+    fontWeight: {
+      light: "300",
+      regular: "400",
+      medium: "500",
+      bold: "700",
+    },
+    letterSpacing: {
+      tighter: "-0.4px",
+      tight: "-0.2px",
+      normal: "normal",
+    },
+    lineHeight: {
+      heading: "1.4em",
+      body: "1.5em",
+    },
+    // Font presets
+    fontSize: {},
   },
-  plugins: [],
+  plugins: [osloTextSizePlugin],
 };
 export default config;

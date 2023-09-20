c99dbb0c0d5ecdb18468540b13e9c1e88190245d
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-20 12:22:51 +0200

    Legg til breakpoints i tw

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      breakpoints: {
+        // min-width
+        mobile: "0px",
+        phablet: "576px",
+        tablet: "768px",
+        "tablet-big": "1024px",
+        laptop: "1280px",
+        desktop: "1600px",
+        // max-width
+        "phablet-max": { max: "576px" },
+        "tablet-max": { max: "768px" },
+        "tablet-big-max": { max: "1024px" },
+        "laptop-max": { max: "1280px" },
+        "desktop-max": { max: "1600px" },
+        // target spesific screen size
+        "mobile-only": { min: "0px", max: "576px" },
+        "phablet-only": { min: "576px", max: "768px" },
+        "tablet-only": { min: "768px", max: "1024px" },
+        "tablet-big-only": { min: "1024px", max: "1280px" },
+        "laptop-only": { min: "1280px", max: "1600px" },
+      },
       fontFamily: {
         sans: ["Oslo sans", "arial", "sans-serif"],
       },

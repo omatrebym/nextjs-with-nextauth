339e67443c6b7499c4c8b534505ef9cafa07f959
omatrebym <ola.matre@bym.oslo.kommune.no>
2023-09-20 07:34:47 +0200

    Legg til innlogging

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { NextAuthProvider } from "@/pages/api/auth/NextAuthProvider";
 
 import Favicon from "@oslokommune/punkt-assets/dist/logos/favicon.ico";
 import Shortcut from "@oslokommune/punkt-assets/dist/logos/16x16-favicon.png";
@@ -24,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="bym-flex bym-flex-col bym-min-h-screen bym-justify-between">
-        <Header />
-        <main className="bym-mt-24">{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <NextAuthProvider>
+      <html lang="en">
+        <body className="bym-flex bym-flex-col bym-min-h-screen bym-justify-between">
+          <Header />
+          <main className="bym-mt-24">{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </NextAuthProvider>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import { PktHeader } from "@oslokommune/punkt-react";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Header() {
+  const { data: session } = useSession();
+
   return (
     <PktHeader
       fixed
       user={{
-        name: "Ola Matre",
-        shortname: "OM",
-        lastLoggedIn: new Date(),
+        name: session?.user?.name ?? "Ikke innlogget",
+        shortname:
+          session?.user?.name
+            ?.split(" ")
+            .reduce((prev, curr) => prev + curr[0].toLocaleLowerCase()) ?? "NN",
       }}
+      logOut={() => signOut()}
+      showLogOutButton
       serviceName="Klagebehandling"
       canChangeRepresentation={false}
     />
